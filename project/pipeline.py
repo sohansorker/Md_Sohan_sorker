d9a0c1cbe2b3603fc3eaf60ae122a2a1349a955a
sohansorker <sohanhasan452@gmail.com>
2024-06-12 15:29:50 +0200

    update pipeline

--- project/pipeline.py
+++ project/pipeline.py
@@ -28,30 +28,78 @@ class Pipeline:
         # Loop through each URL and read data into a DataFrame
         for url in url_list:
             # Use pd.read_csv to read data from the URL
-            df = pd.read_csv(url, sep=';', skiprows=1, skipfooter=0)
-            dataframes.append(df)
+            df_temp = pd.read_csv(url, sep=';', skiprows=1, skipfooter=0)
+            dataframes.append(df_temp)
 
 
         # Concatenate the list of DataFrames into a single DataFrame
-        complete_df = pd.concat(dataframes, ignore_index=True)
+            df = pd.concat(dataframes, ignore_index=True)
 
-        # Filter the combined DataFrame
-        df_filtered = complete_df[(complete_df['Jahr'] >= 2018) & (complete_df['Jahr'] <= 2020)]
-        refined_df = df_filtered.sort_values(by=['Jahr', 'Monat'])
+        #removed unnamed column
+            df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+
+            df = df.drop(columns=['Deutschland'])
+
+        #sorting data
+            df = df.sort_values(by=['Jahr', 'Monat'])
+
+        #rename header
+            df = df.rename(columns={"Jahr": "Year", "Monat": "Month"})
+        #keep data from 2018 to 2020
+            df = df.loc[(df['Year'] >= 2018) & (df['Year'] <= 2020)]
+        #change month value int to month name
+            df['Month'] = df['Month'].replace(
+                            [num for num in range(1, 13)],
+                            [month for month in calendar.month_name[1:]])
+        #creating new column Date with year and month
+            df.insert(loc=2, column="Date", value=df['Month'] + "-" + df['Year'].astype(str)) 
+            df = df.reset_index(drop=True)
 
 
         # Store the temperature data in the database
-        refined_df.to_sql('AvgTemp', self.engine, if_exists='replace', index=False)
+        df.to_sql('AvgTemp', self.engine, if_exists='replace', index=False)
 
     def collect_additional_data(self):
         # collect additional data from another URL
-        url2 = "https://www-genesis.destatis.de/genesisWS/rest/2020/data/tablefile?username=DE9WS28QIO&password=Sohanhasan@123&name=46241-0021&area=all&compress=false&transpose=false&startyear=2018&endyear=2020&language=en"
-        data1 = pd.read_csv(url2, sep=';', skiprows=6, skipfooter=3, engine='python')
-        df = data1
-        df_transposed = df.T
+        url2 = "https://www-genesis.destatis.de/genesisWS/rest/2020/data/tablefile?username=DE9WS28QIO&password=Sohanhasan@123&name=46241-0021&area=all&compress=false&transpose=true&startyear=2018&endyear=2020&language=en"
+        df2 = pd.read_csv(url2, sep=';', skiprows=6, skipfooter=3, engine='python')
+
+        # Deleting rows 0 and 2
+        df2 = df2.drop([0, 2])
+
+        df2 = df2.reset_index(drop=True)
+
+        # Always keep the first two columns
+        always_keep = df2.iloc[:, :2]
+
+        # From the 3rd column onward, apply the condition
+        # Check for columns where both the first and second rows are 'Total'
+        condition_columns = df2.iloc[:, 2:]
+        mask = (condition_columns.iloc[0] == 'Total') & (condition_columns.iloc[1] == 'Total')
+
+        # Filter the DataFrame to keep only those columns
+        filtered_columns = condition_columns.loc[:, mask]
+
+        # Concatenate the always kept columns with the conditionally kept columns
+        final_df = pd.concat([always_keep, filtered_columns], axis=1)
+
+        # Deleting rows 0 and 2
+        final_df = final_df.drop([0, 1])
+        final_df = final_df.reset_index(drop=True)
+
+        final_df.columns = [col.split('.')[0] for col in final_df.columns]
+
+        final_df = final_df.rename(columns={
+            'Unnamed: 0': 'Year',  # New name for 'Unnamed: 0'
+            'Unnamed: 1': 'Month'   # New name for 'Unnamed: 1'
+        })
+
+        final_df['Year'] = final_df['Year'].astype(int)
+
+
 
         # Store the additional data in the same database
-        df_transposed.to_sql('AccData', self.engine, if_exists='replace', index=False)
+        final_df.to_sql('AccData', self.engine, if_exists='replace', index=False)
         self.engine.dispose()
         
 
